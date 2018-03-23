8bf3c0a975a61f881896df0c78b421ef17bdf1cc
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-03-23 12:57:23 +1100

    Add 122 best time to buy and sell stock 2

--- src/_122BestTimeToBuyAndSellStock2.java
+++ src/_122BestTimeToBuyAndSellStock2.java
@@ -1,13 +1,12 @@
 //Say you have an array for which the ith element is the price of a given stock on day i.
-//
 //Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 public class _122BestTimeToBuyAndSellStock2 {
 	public int maxProfit(int[] prices) {
 		int maxPrice = 0;
 		
-		for (int i = 0; i < prices.length - 1; i++) {
-			if (prices[i] < prices[i + 1])
-				maxPrice += prices[i + 1] -prices[i];
+		for (int j = 0; j < prices.length - 1; j++) {
+			if (prices[j] < prices[j + 1])
+				maxPrice += prices[j + 1] -prices[j];
 		}
 		
 		return maxPrice;
