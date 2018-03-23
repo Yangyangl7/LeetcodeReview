bb537a5a923e8cebf67c18017d4c97b524222040
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-03-23 12:13:24 +1100

    Add 121 Best time to buy and sell stock with 1 limitation.

--- src/_121BestTimeToBuyAndSellStock.java
+++ src/_121BestTimeToBuyAndSellStock.java
@@ -1,4 +1,19 @@
-
+//Say you have an array for which the ith element is the price of a given stock on day i.
+//
+//If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 public class _121BestTimeToBuyAndSellStock {
-
+	 public int maxProfit(int[] prices) {
+		 int minPrice = Integer.MAX_VALUE;
+		 int maxPrice = 0;
+		 
+		 for (int i = 0; i < prices.length; i++) {
+			 if (prices[i] < minPrice)
+				 minPrice = prices[i];
+			 else if (prices[i] - minPrice > maxPrice) {
+				 maxPrice =  prices[i] - minPrice;
+			 }
+		 }
+		 
+		 return maxPrice;
+	 }
 }
