118ae1fc95a85c82520d1acd7c15c19675803ddf
Yangyang Luo <31087080+Yangyangl7@users.noreply.github.com>
2018-04-13 15:09:28 +1000

    Add #5 longest Palindormic Substring

---
+++ src/_5longestPalindormicSubstrings.java
@@ -0,0 +1,32 @@
+
+public class _5longestPalindormicSubstrings {
+	public String longest(String s) {
+		int startIndex = 0;
+		int maxlength = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			int l1 = findLength(s, i, i); //odd string length
+			int l2 = findLength(s, i, i+1);//even string length
+			int update = Math.max(l1, l2);
+			
+			if (update > maxlength) {
+				maxlength = update;
+				startIndex = l1 == update? i - maxlength / 2: i - maxlength /2 + 1; 
+			}
+		}
+		
+		return s.substring(startIndex, startIndex + maxlength);
+	}
+	
+	public int findLength(String s, int Left, int Right) {
+		int L = Left;
+		int R = Right;
+
+		while (L > -1 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+			L--;
+			R++;
+		}
+		
+		return R - L - 1;
+	}
+}
